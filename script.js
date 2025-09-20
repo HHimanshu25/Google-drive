73691ae366220435dde34eb3401f0d0ccd5ddcdd
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-09-20 09:42:25 +0530

    update file

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.getElementById("page").addEventListener("click", ()=> {
-            document.querySelector(".add-page").classList.toggle('add-page2')
-        });
-        window.addEventListener("load", ()=> {
-            document.querySelector(".add-page").classList.remove('add-page2')
-        });
-        
-      
+window.addEventListener("load", () => {
+    document.querySelector(".add-page").classList.remove('add-page2')
+});
+document.getElementById("page").addEventListener("click", () => {
+    document.querySelector(".add-page").classList.toggle('add-page2')
+});
+
+document.querySelector(".second").addEventListener('click', ()=>{
+    document.querySelector(".fileinput").click();
+});
